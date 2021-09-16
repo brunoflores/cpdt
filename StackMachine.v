77fe339cbb09096ff0dd731670eaaa5eadfac32c
Bruno Flores <bruno@brunoflores.com.br>
2021-09-17 09:03:40 +1000

    Write target stack language.

--- StackMachine.v
+++ StackMachine.v
@@ -31,3 +31,35 @@ Eval simpl in expDenote (Binop Plus (Const 2) (Const 2)).
 Eval simpl in expDenote (Binop Times (Binop Plus (Const 2) (Const 2)) (Const 7)).
 
 (* ---------- Target language of our compiler ------------- *)
+
+(* Syntax of the stack machine. *)
+
+Inductive instr : Set :=
+| iConst : nat -> instr
+| iBinop : binop -> instr.
+
+Definition prog := list instr.
+Definition stack := list nat.
+
+(* An instruction either pushes a constant onto the stack or pops two arguments,
+   applies a binary operator to them, and pushes the result onto the stack. *)
+Definition instrDenote (i : instr) (s : stack) : option stack :=
+  match i with
+  | iConst n => Some (n :: s)
+  | iBinop b =>
+    match s with
+    | arg1 :: arg2 :: s' => Some ((binopDenote b) arg1 arg2 :: s')
+    | _ => None
+    end
+  end.
+
+(* Iterates application of [instrDenote] through a whole program. *)
+Fixpoint progDenote (p : prog) (s : stack) : option stack :=
+  match p with
+  | nil => Some s
+  | i :: p' =>
+    match instrDenote i s with
+    | None => None
+    | Some s' => progDenote p' s'
+    end
+  end.
