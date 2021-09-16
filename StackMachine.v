57583cba8a4224270fd05562deaa45314c48137b
Bruno Flores <bruno@brunoflores.com.br>
2021-09-17 08:41:25 +1000

    Add initial syntax and meaning.

--- StackMachine.v
+++ StackMachine.v
@@ -1,3 +1,19 @@
 Require Import Bool Arith List Lib.CpdtTactics.
 Set Implicit Arguments.
 Set Asymmetric Patterns.
+
+(* Syntax of the source language. *)
+Inductive binop : Set := Plus | Times.
+(* Use Set to signal as a constituent of programs. *)
+
+(* Our type of arithmetic expressions. *)
+Inductive exp : Set :=
+| Const : nat -> exp
+| Binop : binop -> exp -> exp -> exp.
+
+(* What the programs mean. *)
+Definition binopDenote (b : binop) : nat -> nat -> nat :=
+  match b with
+  | Plus => plus
+  | Times => mult
+  end.
